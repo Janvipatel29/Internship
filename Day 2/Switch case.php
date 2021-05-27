133ff9f3048a60a12b0c34bd2e8f2eb552f24a92
Janvipatel29 <84802201+Janvipatel29@users.noreply.github.com>
2021-05-27 13:50:05 +0530

    demo

---
+++ Day 2/Switch case.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 1;
+switch ($a) 
+{
+    case 1:
+        echo "It is one";
+        break;
+    case 2:
+        echo "It is Two";
+        break;
+    case 3:
+        echo "It is Three";
+        break;
+    default:
+        echo "Please Select 1-3";
+        break;
+}
+?>
