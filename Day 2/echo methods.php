0f45942b11eefe89120a14888980452a6c6674d2
Janvipatel29 <84802201+Janvipatel29@users.noreply.github.com>
2021-05-27 13:19:52 +0530

    Create echo methods.php

---
+++ Day 2/echo methods.php
@@ -0,0 +1,23 @@
+<?php
+
+//variable
+//Method 1
+$a = 10;
+$b = 20;
+
+//method 1
+echo "A Value is ".$a;
+echo "<br/>B Value is ".$b;
+       
+//method 2
+echo "<br/>A Value is ".$a."<br/>B Value is ".$b;
+
+//method 3
+echo "<br/>A Value is $a<br/>B Value is $b";
+
+//method 4
+echo '<br/>A Value is $a<br/>B Value is $b';
+
+//method 5
+echo "<br/>A Value is $a<br/>B Value is $b<br/>Sum is ".($a+$b);
+?>
