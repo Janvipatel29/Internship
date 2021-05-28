40b3eb021e565ddbfd543b36690941e99210f301
Janvipatel29 <84802201+Janvipatel29@users.noreply.github.com>
2021-05-28 15:37:02 +0530

    task

---
+++ Day 2/leap year.php
@@ -0,0 +1,14 @@
+<?php
+$value;
+$value = 2012;
+echo "Enter a year <br>";
+echo "$value <br>";
+if($value % 4 == 0)
+{
+    echo "$value is a leap year";
+}
+else
+{
+	echo "$value is Not a leap year";
+}
+?>
